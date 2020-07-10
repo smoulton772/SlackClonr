43518a64b36bebf1ba0c49b8316eada0135cb3d2
Sean Moulton <s.moulton772@gmail.com>
2020-07-10 13:08:34 -0400

    passed redux state via props

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -6,6 +6,7 @@ import {Menu} from 'semantic-ui-react';
 
 class SidePanel extends React.Component {
     render() {
+        const {currentUser} = this.props;
         return(
            <Menu
            size='large'
@@ -15,7 +16,7 @@ class SidePanel extends React.Component {
            style={{background : '#4A154B', fontSize: '1.2rem'}}
            >
 
-            <UserPanel />
+            <UserPanel currentUser={currentUser} />
            </Menu>
         )
     }
--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,52 +1,68 @@
-import React from 'react';
-import {Grid, Header, Icon, Dropdown} from 'semantic-ui-react'
-import firebase from '../../firebase'
+import React, { useReducer } from "react";
+import { Grid, Header, Icon, Dropdown } from "semantic-ui-react";
+import firebase from "../../firebase";
 
 class UserPanel extends React.Component {
-    
-    dropdownOptions = () => [
-        {
-            key:'user',
-            text: <span>Signed in as<strong>User</strong></span>,
-            disabled: true
-        },
-        {
-            key:'avatar',
-            text:<span>Change Avatar</span>
-        },
-        {
-            key: 'sign out',
-            text:<span onClick={this.handleSignout}>Sign Out</span>
-        },
-    ]
-    
-    handleSignout = () => {
-        firebase
-        .auth()
-        .signOut()
-        .then(() => console.log("signed out!") )
-    }
-    
-    render(){
-        return(
-            <Grid style={{background: '#4A154B'}}>
-                <Grid.Column>
-                    <Grid.Row style={{padding: '1.2em', margin: 0}}>
-                        {/* App Header */}
-                        <Header inverted flaoted ="left" as="h2">
-                            <Icon name="modx"/>
-                            <Header.Content>DevChat</Header.Content>
-                        </Header>
-                    </Grid.Row>
-{/* User Dropdown */}
-
-<Header style={{padding: '0.25'}} as="h4" inverted>
-    <Dropdown trigger={<span>User</span>} options={this.dropdownOptions()} / >
-</Header>
-                </Grid.Column>
-            </Grid>
-        )
-    }
+state = {
+    user: this.props.currentUser
 }
+
+
+
+  dropdownOptions = () => [
+    {
+      key: "user",
+      text: (
+        <span>
+        Signed in as<strong>{this.state.user.displayName}</strong>
+        </span>
+      ),
+      disabled: true,
+    },
+    {
+      key: "avatar",
+      text: <span>Change Avatar</span>,
+    },
+    {
+      key: "sign out",
+      text: <span onClick={this.handleSignout}>Sign Out</span>,
+    },
+  ];
+
+  handleSignout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => console.log("signed out!"));
+  };
+
+  render() {
+    // console.log(this.props.currentUser);
+
+    return (
+      <Grid style={{ background: "#4A154B" }}>
+        <Grid.Column>
+          <Grid.Row style={{ padding: "1.2em", margin: 0 }}>
+            {/* App Header */}
+            <Header inverted flaoted="left" as="h2">
+              <Icon name="modx" />
+              <Header.Content>DevChat</Header.Content>
+            </Header>
+          </Grid.Row>
+          {/* User Dropdown */}
+
+          <Header style={{ padding: "0.25" }} as="h4" inverted>
+            <Dropdown
+              trigger={<span>{this.state.user.displayName}</span>}
+              options={this.dropdownOptions()}
+            />
+          </Header>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+
 
 export default UserPanel;
